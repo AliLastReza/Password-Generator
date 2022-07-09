d95f9409ae8595f6bd78b82d9a569d11c13196da
Mohammad Ali Reza <AliLastReza@pm.me>
2022-07-09 17:32:36 +0430

    ArgumentParser added and used
    settings.py added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
 import string
 
 from random import choices
+
+from settings import *
 
 
 def generate_password(length=8, use_upper=False, use_lower=False, use_digit=False, use_symbol=False):
@@ -26,4 +29,19 @@ def generate_password(length=8, use_upper=False, use_lower=False, use_digit=Fals
 
 
 if __name__ == '__main__':
-    print(generate_password(length=20, use_upper=True, use_lower=True, use_digit=True, use_symbol=True))
+    parser = argparse.ArgumentParser(prog=PROGRAM_NAME,
+                                     epilog=EPILOG_MSG,
+                                     fromfile_prefix_chars='@',
+                                     description=DESCRIPTION,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-len', '--length', type=int, default=DEFAULT_LENGTH, help="Length of password")
+    parser.add_argument('-up', '--upper', help="Use upper case characters", action='store_true')
+    parser.add_argument('-lw', '--lower', help="Use lower case characters", action='store_true')
+    parser.add_argument('-d', '--digit', help="Use digits", action='store_true')
+    parser.add_argument('-s', '--symbol', help="Use symbols", action='store_true')
+
+    args = parser.parse_args()
+
+    print(generate_password(length=args.length, use_upper=args.upper, use_lower=args.lower, use_digit=args.digit,
+                            use_symbol=args.symbol))
---
+++ settings.py
@@ -0,0 +1,9 @@
+VERSION = '1.0.0'
+DESCRIPTION = f"""Password Generator v{VERSION}
+Generates a random password as you want."""
+
+PROGRAM_NAME = 'password-generator'
+EPILOG_MSG = """Wish you an awesome day.
+Copyright 2022, Mohammad Ali Reza"""
+
+DEFAULT_LENGTH = 8
